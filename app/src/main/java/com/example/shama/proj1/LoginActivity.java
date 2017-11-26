d7189343742dd296ddb5c6c221fc5837c9ce1395
Basmah1 <basmah1@github.com>
2017-11-26 19:31:40 +0400

    some change

--- app/src/main/java/com/example/shama/proj1/LoginActivity.java
+++ app/src/main/java/com/example/shama/proj1/LoginActivity.java
@@ -1,21 +1,36 @@
 package com.example.shama.proj1;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
     EditText logName;
     EditText logID;
-    Button ok;
+    Button login;
+
+    public void homeB(){
+        login = (Button) findViewById(R.id.loginB);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent (LoginActivity.this,Home.class);
+                startActivity(i);
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         logName = (EditText) findViewById(R.id.Name);
         logID = (EditText) findViewById(R.id.ID);
-        ok =(Button) findViewById(R.id.button6);
+        login =(Button) findViewById(R.id.loginB);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        homeB();
     }
 }
